2e003ea5c869e9e8b360414495a31865d0849626
alextanhongpin <alextan220990@gmail.com>
2022-06-13 14:45:01 +0800

    chore: cleanup types

--- pkg/chat/chat.go
+++ pkg/chat/chat.go
@@ -21,9 +21,9 @@ type authorizer interface {
 	Verify(token string) (string, error)
 }
 
-type Chat[T Message] struct {
-	redis *socketio.IORedis[T]
-	io    *socketio.IO[T]
+type Chat struct {
+	redis *socketio.IORedis[Message]
+	io    *socketio.IO[Message]
 	done  chan struct{}
 	wg    sync.WaitGroup
 	evtCh chan event
@@ -31,11 +31,11 @@ type Chat[T Message] struct {
 	mu    sync.RWMutex
 }
 
-func New[T Message](channel string, client *redis.Client, authz authorizer) (*Chat[T], func()) {
-	io, closeio := socketio.NewIO[T]()
-	ioredis, closeredis := socketio.NewIORedis[T](channel, client)
+func New(channel string, client *redis.Client, authz authorizer) (*Chat, func()) {
+	io, closeio := socketio.NewIO[Message]()
+	ioredis, closeredis := socketio.NewIORedis[Message](channel, client)
 
-	chat := &Chat[T]{
+	chat := &Chat{
 		redis: ioredis,
 		io:    io,
 		done:  make(chan struct{}),
@@ -51,7 +51,7 @@ func New[T Message](channel string, client *redis.Client, authz authorizer) (*Ch
 	}
 }
 
-func (c *Chat[T]) ServeWS(w http.ResponseWriter, r *http.Request) {
+func (c *Chat) ServeWS(w http.ResponseWriter, r *http.Request) {
 	token := r.URL.Query().Get("token")
 
 	username, err := c.authz.Verify(token)
@@ -83,44 +83,34 @@ func (c *Chat[T]) ServeWS(w http.ResponseWriter, r *http.Request) {
 	}()
 
 	for msg := range socket.Listen() {
-		m, ok := any(msg).(Message)
-		if !ok {
-			panic("invalid message type")
-		}
-		m.From = username
+		msg.From = username
 
 		users, _ := friends[username]
 		for _, user := range users {
-			msg := m
-			msg.To = user
-			if err := c.emitRemote(msg); err != nil {
+			m := msg
+			m.To = user
+
+			if err := c.emitRemote(m); err != nil {
 				panic(err)
 			}
 		}
 	}
 }
 
-func (c *Chat[T]) loop() {
+func (c *Chat) loop() {
 	for {
 		select {
 		case <-c.done:
 			return
-
 		case evt := <-c.evtCh:
 			c.eventProcessor(evt)
-			// Read the remote redis and publish to all local nodes.
 		case msg := <-c.redis.Subscribe():
-			switch m := any(msg).(type) {
-			case Message:
-				c.emitLocal(m)
-			default:
-				panic(fmt.Errorf("chat: unhandled message subscription: %+v", msg))
-			}
+			c.emitLocal(msg)
 		}
 	}
 }
 
-func (c *Chat[T]) loopAsync() {
+func (c *Chat) loopAsync() {
 	c.wg.Add(1)
 
 	go func() {
@@ -130,7 +120,7 @@ func (c *Chat[T]) loopAsync() {
 	}()
 }
 
-func (c *Chat[T]) eventProcessor(evt event) {
+func (c *Chat) eventProcessor(evt event) {
 	switch e := evt.(type) {
 	case Connected:
 		c.connected(e)
@@ -141,27 +131,26 @@ func (c *Chat[T]) eventProcessor(evt event) {
 	}
 }
 
-func (c *Chat[T]) addSession(username, sessionID string) error {
+func (c *Chat) addSession(username, sessionID string) error {
 	return c.redis.Client.SAdd(context.Background(), username, []string{sessionID}).Err()
 }
 
-func (c *Chat[T]) removeSession(username, sessionID string) error {
+func (c *Chat) removeSession(username, sessionID string) error {
 	return c.redis.Client.SRem(context.Background(), username, []string{sessionID}).Err()
 }
 
-func (c *Chat[T]) getSessions(username string) ([]string, error) {
+func (c *Chat) getSessions(username string) ([]string, error) {
 	return c.redis.Client.SMembers(context.Background(), username).Result()
 }
 
-func (c *Chat[T]) fetchFriends(username string) {
+func (c *Chat) fetchFriends(username string) {
 	users := friends[username]
 
 	statuses := make([]Friend, len(users))
 	for i, user := range users {
 		statuses[i] = Friend{
 			Username: user,
-			// TODO: ̧Check over the redis.
-			Online: c.isUserOnline(user),
+			Online:   c.isUserOnline(user),
 		}
 	}
 
@@ -173,7 +162,7 @@ func (c *Chat[T]) fetchFriends(username string) {
 	})
 }
 
-func (c *Chat[T]) notifyPresence(username string, online bool) {
+func (c *Chat) notifyPresence(username string, online bool) {
 	users := friends[username]
 
 	for _, user := range users {
@@ -190,7 +179,7 @@ func (c *Chat[T]) notifyPresence(username string, online bool) {
 	}
 }
 
-func (c *Chat[T]) connected(evt Connected) {
+func (c *Chat) connected(evt Connected) {
 	if err := c.addSession(evt.username, evt.sessionID); err != nil {
 		c.emitError(evt.sessionID, err)
 
@@ -201,7 +190,7 @@ func (c *Chat[T]) connected(evt Connected) {
 	c.notifyPresence(evt.username, true)
 }
 
-func (c *Chat[T]) disconnected(evt Disconnected) {
+func (c *Chat) disconnected(evt Disconnected) {
 	if err := c.removeSession(evt.username, evt.sessionID); err != nil {
 		c.emitError(evt.sessionID, err)
 
@@ -211,16 +200,16 @@ func (c *Chat[T]) disconnected(evt Disconnected) {
 	c.notifyPresence(evt.username, c.isUserOnline(evt.username))
 }
 
-func (c *Chat[T]) isUserOnline(username string) bool {
+func (c *Chat) isUserOnline(username string) bool {
 	sessions, err := c.getSessions(username)
 	return err == nil && len(sessions) > 0
 }
 
-func (c *Chat[T]) emitError(sessionID string, err error) bool {
-	return c.io.EmitAny(sessionID, err)
+func (c *Chat) emitError(sessionID string, err error) bool {
+	return c.io.Error(sessionID, err)
 }
 
-func (c *Chat[T]) emitLocal(msg Message) bool {
+func (c *Chat) emitLocal(msg Message) bool {
 	sessionIDs, _ := c.getSessions(msg.To)
 
 	for _, sid := range sessionIDs {
@@ -230,6 +219,6 @@ func (c *Chat[T]) emitLocal(msg Message) bool {
 	return true
 }
 
-func (c *Chat[T]) emitRemote(msg any) error {
+func (c *Chat) emitRemote(msg any) error {
 	return c.redis.Publish(context.Background(), msg)
 }
